d224232a042a5d340089b141697d2915ad332a1a
Mohamedlemin <med3744lemin@gmail.com>
2021-10-24 11:41:51 +0000

    Creation du model

--- Portfolio_Med/urls.py
+++ Portfolio_Med/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+import experience.views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', experience.views.index, name='index'),
+
 ]
