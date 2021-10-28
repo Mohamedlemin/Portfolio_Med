944ff29f38dc08b90d64e0e769af3b9bffe351a3
Mohamedlemin <med3744lemin@gmail.com>
2021-10-28 12:05:26 +0000

    Front-end

--- Portfolio_Med/urls.py
+++ Portfolio_Med/urls.py
@@ -1,4 +1,4 @@
-"""Portfolio_Med URL Configuration
+"""portfolio URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -15,9 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 import experience.views
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', experience.views.index, name='index'),
 
+    # path('', jobs.views.Home, name='Home'),
+   # path('jobs/<int:job_id>', jobs.views.detail, name='detail'),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- experience/views.py
+++ experience/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import experience
 # Create your views here.
+
+# First comment
 def index(request):
     experiences = experience.objects
     return render(request, 'experience/index.html', {'experiences': experiences})
