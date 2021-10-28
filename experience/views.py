35b761bbe8c644310c972175a143cb6b436f4b52
Mohamedlemin <med3744lemin@gmail.com>
2021-10-28 12:07:26 +0000

    second comment

--- experience/views.py
+++ experience/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import experience
 # Create your views here.
 
-# second comment
+# second comment test commit
 def index(request):
     experiences = experience.objects
     return render(request, 'experience/index.html', {'experiences': experiences})
